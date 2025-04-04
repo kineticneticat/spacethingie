51d4761a4f5d2818e3ba2fd2ef528eb761a3dabb
kineticneticat <84404038+kineticneticat@users.noreply.github.com>
2025-04-04 09:03:24 +0100

    Revert "this is why we remember to push"
    This reverts commit 2dfa295d51eecc72c742fba00be2a34311839da1, reversing
    changes made to b0b0195ec69bcdd671952e67e66ce2d1574d4034.

--- marker.gd
+++ marker.gd
@@ -1,19 +1,16 @@
 extends Node3D
 
 @export var Name = "Blank"
-@export var Colour = Color(0.5,1,1,1)
-@export var Target: Node3D
-@export var z = 0
 
 const uipartnode = preload("res://ui_marker_part.tscn")
 @onready var MarkersNode = get_node("/root/World/UI/Markers")
+@onready var Player: CharacterBody3D = get_node("/root/World/Player")
 
 var UIPart: Control
 
 func _ready() -> void:
 	UIPart = uipartnode.instantiate()
-	(UIPart.get_node("Cross") as TextureRect).modulate = Colour
-	UIPart.z_index = z
+	(UIPart.get_node("Cross") as TextureRect).modulate = Color(0.5,1,1,1)
 	MarkersNode.add_child(UIPart)
 
 func _process(delta: float) -> void:
@@ -21,6 +18,6 @@ func _process(delta: float) -> void:
 		UIPart.visible = true
 		UIPart.position = get_viewport().get_camera_3d().unproject_position(global_position)
 		(UIPart.get_node("Name") as Label).text = Name
-		(UIPart.get_node("Distance") as Label).text = str(round(global_position.distance_to(Target.global_position)*10)/10) + "m"
+		(UIPart.get_node("Distance") as Label).text = str(round(global_position.distance_to(Player.global_position)*10)/10) + "m"
 	else:
 		UIPart.visible = false
